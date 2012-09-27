3df4ba6de5ec95a9705d44e6dc7d7d39341f26c5
Daniel Braithwaite <contact.dtb@gmail.com>
2012-09-27 03:42:46 +0200

    now retains class structure; needs scrutiny

--- collection-merge.R
+++ collection-merge.R
@@ -1,31 +1,35 @@
-setMethod("+", signature("collection", "collection"), 
+setMethod ("+", signature ("collection", "collection"), 
           function (e1, e2) {
-            mm <- merge (e1$count, e2$count, all = TRUE, by = 0, sort = TRUE)
-            rownames (mm) <- mm$Row.names
-            mm$Row.names <- NULL
-            as.matrix (mm)
+#             mm <- merge (e1$count, e2$count, all = TRUE, by = 0, sort = TRUE)
+#             rownames (mm) <- mm$Row.names
+#             mm$Row.names <- NULL
+#             as.matrix (mm)
+            
+            if (!identical (viewnames (e1), viewnames(e2)))
+              warning ("only views with names in common will be retained")
 
-#             if (!identical (viewnames (e1), viewnames(e2)))
-#               warning ("only views in common will be retained")
-#             
-#             cc <- new("collection")
-#             cc@sel <- new ("selection",
-#                            ids = append (e1@sel@ids, e2@sel@ids)
-### must check for default grouping
-#                            groups = as.factor (append (groups (e1), groups (e2)))
-#                            metadata = ...,
-#                            ids.spec = ...,
-#                            resources.spec = ...,
-#                            metadata.extent = ...)
-#             names (cc) <- append (names (e1), names (e2))
-#             cc@views <- intersect (views (e1), views (e2))
-#             viewnames (cc) <- intersect (viewnames (e1), viewnames (e2))
-# 
-#             for (v in viewnames (cc)) {
-#               cc@data [[v]]  <- merge (e1@data [[v]], e2@data [[v]], all = TRUE, by = 0, sort = TRUE)
-#               rownames (cc@data [[v]]) <- cc@data [[v]] $Row.names
-#               cc@data [[v]] $Row.names <- NULL
-#               cc@data [[v]] <- new ("mmatrix", data = cc@data [[v]])
-#           }
-#           cc
-} )
+            cc <- new("collection")
+            cc@sel <- new ("selection",
+                           ids = append (e1@sel@ids, e2@sel@ids),
+                           groups = as.factor (append (groups (e1), groups (e2))))
+            ### need to fix metadata     metadata = ...,
+            #                            ids.spec = ...,
+            #                            resources.spec = ...,
+            #                            metadata.extent = ...)
+            names (cc) <- append (names (e1), names (e2))
+            v <- intersect (viewnames (e1), viewnames (e2))
+            v1 <- views (e1) [v]
+            v2 <- views (e2) [v]
+            if (!identical (v1, v2))
+              warning ("possibly merging views with same name that are not same")
+            cc@views <- v1
+            cc@data <- list()
+            for (j in v) {
+              m <- merge (e1@data [[j]], e2@data [[j]], all = TRUE, by = 0, sort = TRUE)
+              rownames (m) <- m$Row.names
+              m$Row.names <- NULL
+              cc@data [[j]] <- new ("mmatrix", data = Matrix::Matrix(as.matrix(m)))
+            }
+            viewnames (cc) <- v
+            cc
+          } )
